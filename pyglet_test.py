3f24ae1b43bca93794dd76dfd041b937a4726403
fargalaxy <far.galaxy.klg@gmail.com>
2020-02-23 14:19:13 +0300

    delete useless code i will use fluidsynth

--- pyglet_test.py
+++
@@ -1,6 +0,0 @@
-import pyglet
-from time import sleep
-player = pyglet.media.Player()
-song = pyglet.media.load('samples/Piano_C5.wav')
-player.queue(song)
-player.play()
--- simplea.py
+++
@@ -1,6 +0,0 @@
-import simpleaudio as sa
-
-i = input()
-wave_obj = sa.WaveObject.from_wave_file("samples/Piano_C5.wav")
-play_obj = wave_obj.play()
-play_obj.wait_done()
