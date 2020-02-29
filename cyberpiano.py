01f899e792e9bb1ca0e6261782ce3e20ce3b5465
fargalaxy <far.galaxy.klg@gmail.com>
2020-02-29 20:56:55 +0300

    delete unless script remake instruction

--- cyberpiano.py
+++ cyberpiano.py
@@ -1,169 +1,148 @@
-import pygame
-import serial
-import os, sys
-from cp_obj import *
-from mingus.midi import fluidsynth 
+from PyQt5 import QtCore, QtWidgets, QtSerialPort
+from PyQt5.QtGui import QIcon, QFont
+import fluidsynth
+import sys, os
 
-#---------------------Caption-----------------------------
-pygame.init()
-size=[800,600]
-screen=pygame.display.set_mode(size)
-pygame.display.set_caption("CyberPiano")
-programIcon = pygame.image.load('icon.png')
-pygame.display.set_icon(programIcon)
-clock=pygame.time.Clock()
+print(os.path.abspath("soundfonts/"))
 
+#-----------------------------Init Synth----------------------------------------------
+synth = fluidsynth.Synth(1)
+#Set gain to 1 in pyfluinsynth
+#print(synth.main_volume(1, 0))
 
-#---------------------Colors-----------------------------
-white = (255, 255, 255)
-black = (  0,   0,   0)
-bg =    (100, 100, 100)
-
-
-files = os.listdir(os.path.abspath("soundfonts/"))
-
-
-done=False
-cur_sf2 = 0
-cur_port = 0
-instrument = 0
-selected_port = False
-loaded_sf = False
-m_cl = False
-last_note = 0
-ports = []
-
-
-# Check system
 if sys.platform.startswith('win'):
-    for i in range(2, 256):
-        try:
-            port = 'COM%s' % i
-            ser = serial.Serial(port)
-            ser.close()
-            ports.append(port)
-        except serial.serialutil.SerialException :
-            pass        
-    driver = "dsound"
+    synth.start("dsound")
     
 elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
-    for i in range(0, 64):
-        try:
-            port = '/dev/ttyUSB%d' % i
-            ser = serial.Serial(port)
-            ser.close()
-            ports.append(port)
-        except serial.serialutil.SerialException :
-            pass         
+    synth.start("alsa")
     
-    #ports = glob.glob('/dev/tty[A-Za-z]*')
-    driver = "alsa"
-    
-print(ports)
-synth = fluidsynth.FluidSynthSequencer()
-synth.init()
-synth.start_audio_output(driver)
+
+#synth.load_sound_font('soundfonts/QUEST_O_TIME.SF2')
+#synth.set_instrument(0, 0)
 
 
-#--------------------------------Buttons-----------------------------------------    
-but_port = [Button(screen, 250, 25, 35, 35, white, "<", 32, black), 
-            Button(screen, 300, 25, 35, 35, white, ">", 32, black),
-            Button(screen, 350, 25, 150, 35, white, "Connect", 32, black)]
 
-but_sf = [Button(screen, 25, 210, 100, 100, white, "<", 72, black), 
-          Button(screen, 145, 210, 100, 100, white, ">", 72, black),
-          Button(screen, 25, 330, 220, 100, white, "Load", 48, black)]
 
-but_ins = [Button(screen, 275, 210, 100, 100, white, "<", 72, black), 
-          Button(screen, 395, 210, 100, 100, white, ">", 72, black)]
- 
+class Widget(QtWidgets.QWidget):
+    def __init__(self, parent=None):
+        super(Widget, self).__init__(parent)
+        
+        self.soundbank = None
+        self.instrument = 0
+        
+        #-----------------Title---------------------------
+        self.setWindowTitle('CyberPiano')
+        self.setWindowIcon(QIcon('icon.png'))
+        self.resize(800, 600)
+        self.setFont(QFont("Calibri", 16, QFont.Normal))
+        
+        # Output textbox
+        self.infotxt = QtWidgets.QTextEdit(readOnly=True)
+        self.infotxt_ = QtWidgets.QLabel('Information:', self)
+        
+        # Ports List
+        ports = self.find_ports()
+        self.current_port = ports[0]
+        
+        self.ports_box = QtWidgets.QComboBox(self)
+        for i in ports:
+            self.ports_box.addItem(i)   
+        self.ports_box.activated[str].connect(self.set_port) 
+        self.ports_box_ = QtWidgets.QLabel('Port:', self)
+        
+        # Instruments List
+        self.instr_box = QtWidgets.QComboBox(self)
+        for i in range(0, 128):
+            self.instr_box.addItem(str(i)) 
+        self.instr_box.activated[str].connect(self.set_instrument)  
+        self.instr_box.setEnabled(False)
+        self.instr_box_ = QtWidgets.QLabel('Instrument:', self)
+        
+        
+        # Connect button
+        self.button = QtWidgets.QPushButton(text="Connect", checkable=True, toggled=self.on_toggled)
+        
+        self.open_button = QtWidgets.QPushButton("Open .sf2", self)
+        self.open_button.clicked.connect(self.open_file)
+        
+        
+        
+        lay = QtWidgets.QVBoxLayout(self)
+        
+        hlay1 = QtWidgets.QHBoxLayout()
+        hlay1.addWidget(self.ports_box_)
+        hlay1.addWidget(self.ports_box)
+        hlay1.addWidget(self.button)
+        
+        
+        hlay2 = QtWidgets.QHBoxLayout()
+        hlay2.addWidget(self.instr_box_)
+        hlay2.addWidget(self.instr_box)
+        hlay2.addWidget(self.open_button)
+        
+        
+        lay.addLayout(hlay1)
+        lay.addLayout(hlay2)
+        lay.addStretch(1)
+        lay.addWidget(self.infotxt_)
+        lay.addWidget(self.infotxt)
+        
+        
+        
 
-while done==False:
-    screen.fill(bg)
+        self.serial = QtSerialPort.QSerialPort(self.current_port, baudRate=QtSerialPort.QSerialPort.Baud115200, readyRead=self.receive)
+     
+    def set_instrument(self, instr):
+        self.instrument = instr
+        synth.program_select(0, self.soundbank, 0, int(instr))
+        self.infotxt.append("Set instrument: " + str(instr))
+        
+    def open_file(self):
+        file = QtWidgets.QFileDialog.getOpenFileName(self, "Open .sf2 Soundbank", os.path.abspath("soundfonts/"), "Soundbanks (*.sf2)")
+        self.soundbank = synth.sfload(file[0])
+        self.infotxt.append("Opened soundfont: " + file[0])
+        self.instr_box.setEnabled(True)
+        synth.program_select(0, self.soundbank, 0, 0)     
     
-    
-    # ---- Events start -----------
-    for event in pygame.event.get(): # User did something
-        if event.type == pygame.QUIT: # If user clicked close
-            done=True # Flag that we are done so we exit this loop
-        if event.type == pygame.MOUSEBUTTONDOWN: 
-            m_cl=True
-	
-    
-    
-    mouse = pygame.mouse.get_pos()
-    
-    if m_cl:
-        # Port select
-        if but_port[0].overed(mouse):
-            cur_port = (cur_port-1) % len(ports)
-            
-        if but_port[1].overed(mouse):
-            cur_port = (cur_port+1) % len(ports)
-            
-        if but_port[2].overed(mouse) and not selected_port:  
-            try:
-                ser = serial.Serial(ports[cur_port], 115200)
-                but_port[2].color = (0, 255, 0)
-                but_port[2].text = "Connected"
-                selected_port = True
-            except:
-                but_port[2].color = (255, 0, 0)
-                
-        #SF2 select
-        if but_sf[0].overed(mouse):
-            cur_sf2 = (cur_sf2-1) % len(files)
-            but_sf[2].color = white
-            
-        if but_sf[1].overed(mouse):
-            cur_sf2 = (cur_sf2+1) % len(files)
-            but_sf[2].color = white
-         
-        if but_sf[2].overed(mouse): 
-            synth.load_sound_font('soundfonts/' + files[cur_sf2])
-            but_sf[2].color = (0, 255, 0)
-            instrument = 0
-            synth.set_instrument(0, 0)
-            
+    def set_port(self, port):
+        self.current_port = port
+        self.serial.setPortName(port)
+        
+    def find_ports(self):
+        port = []
+        for info in QtSerialPort.QSerialPortInfo().availablePorts():
+            #print(info.portName())
+            port.append(info.portName())
+        return port
 
-         
-         #Instrument select   
-        if but_ins[0].overed(mouse):
-            instrument = (instrument-1) % 256
-            synth.set_instrument(0, instrument)
-             
-        if but_ins[1].overed(mouse):
-            instrument = (instrument+1) % 256 
-            synth.set_instrument(0, instrument)
-                
-    m_cl = False
-    
-    # Drawning
-    draw_text(screen, "Preset:", 32, 25, 100)
-    draw_text(screen, files[cur_sf2][:-4], 32, 25, 150)
-    draw_text(screen, "Port: "+ports[cur_port], 32, 25, 25)
-    draw_text(screen, "Note: "+str(last_note), 32, 525, 100)
-    draw_text(screen, "Instrument: "+str(instrument), 32, 525, 150)
-    
-    for i in but_port: i.draw()
-    for i in but_sf: i.draw()  
-    for i in but_ins: i.draw() 
-    pygame.display.flip()
-    
-    # Read piano
-    if selected_port:
-        if(ser.in_waiting >0):
-            cmd, pitch, velocity = ser.read(), ser.read(), ser.read()
-            if (cmd[0]==144):
-                if (velocity[0]==127):
-                    last_note = pitch[0]
-                    synth.play_event(pitch[0],0,100)
-                    
-                if (velocity[0]==0):
-                    synth.play_event(pitch[0],0,0)
-      
-    
-                
-    
-	
-pygame.quit()
+    @QtCore.pyqtSlot()
+    def receive(self):
+        while self.serial.bytesAvailable():
+            cmd = self.serial.read(3)
+            #text = text.rstrip('\r\n')
+            if cmd[0] == 144:
+                self.infotxt.append("Note: " + str(cmd[1]) + "   Velocity: " + str(cmd[2]))
+                synth.noteon(0, cmd[1], cmd[2])
+
+    @QtCore.pyqtSlot()
+    def send(self):
+        self.serial.write(self.message_le.text().encode())
+
+    @QtCore.pyqtSlot(bool)
+    def on_toggled(self, checked):
+        self.button.setText("Disconnect" if checked else "Connect")
+        if checked:
+            if not self.serial.isOpen():
+                if not self.serial.open(QtCore.QIODevice.ReadWrite):
+                    self.button.setChecked(False)
+                    self.infotxt.append("Connected to port: " + str(self.current_port))
+        else:
+            self.serial.close()
+            self.infotxt.append("Disconnected port: " + str(self.current_port))
+
+if __name__ == '__main__':
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    w = Widget()
+    w.show()
+    sys.exit(app.exec_())
---
+++ cyberpiano_old.py
@@ -0,0 +1,169 @@
+import pygame
+import serial
+import os, sys
+from cp_obj import *
+from mingus.midi import fluidsynth 
+
+#---------------------Caption-----------------------------
+pygame.init()
+size=[800,600]
+screen=pygame.display.set_mode(size)
+pygame.display.set_caption("CyberPiano")
+programIcon = pygame.image.load('icon.png')
+pygame.display.set_icon(programIcon)
+clock=pygame.time.Clock()
+
+
+#---------------------Colors-----------------------------
+white = (255, 255, 255)
+black = (  0,   0,   0)
+bg =    (100, 100, 100)
+
+
+files = os.listdir(os.path.abspath("soundfonts/"))
+
+
+done=False
+cur_sf2 = 0
+cur_port = 0
+instrument = 0
+selected_port = False
+loaded_sf = False
+m_cl = False
+last_note = 0
+ports = []
+
+
+# Check system
+if sys.platform.startswith('win'):
+    for i in range(2, 256):
+        try:
+            port = 'COM%s' % i
+            ser = serial.Serial(port)
+            ser.close()
+            ports.append(port)
+        except serial.serialutil.SerialException :
+            pass        
+    driver = "dsound"
+    
+elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+    for i in range(0, 64):
+        try:
+            port = '/dev/ttyUSB%d' % i
+            ser = serial.Serial(port)
+            ser.close()
+            ports.append(port)
+        except serial.serialutil.SerialException :
+            pass         
+    
+    #ports = glob.glob('/dev/tty[A-Za-z]*')
+    driver = "alsa"
+    
+print(ports)
+synth = fluidsynth.FluidSynthSequencer()
+synth.init()
+synth.start_audio_output(driver)
+
+
+#--------------------------------Buttons-----------------------------------------    
+but_port = [Button(screen, 250, 25, 35, 35, white, "<", 32, black), 
+            Button(screen, 300, 25, 35, 35, white, ">", 32, black),
+            Button(screen, 350, 25, 150, 35, white, "Connect", 32, black)]
+
+but_sf = [Button(screen, 25, 210, 100, 100, white, "<", 72, black), 
+          Button(screen, 145, 210, 100, 100, white, ">", 72, black),
+          Button(screen, 25, 330, 220, 100, white, "Load", 48, black)]
+
+but_ins = [Button(screen, 275, 210, 100, 100, white, "<", 72, black), 
+          Button(screen, 395, 210, 100, 100, white, ">", 72, black)]
+ 
+
+while done==False:
+    screen.fill(bg)
+    
+    
+    # ---- Events start -----------
+    for event in pygame.event.get(): # User did something
+        if event.type == pygame.QUIT: # If user clicked close
+            done=True # Flag that we are done so we exit this loop
+        if event.type == pygame.MOUSEBUTTONDOWN: 
+            m_cl=True
+	
+    
+    
+    mouse = pygame.mouse.get_pos()
+    
+    if m_cl:
+        # Port select
+        if but_port[0].overed(mouse):
+            cur_port = (cur_port-1) % len(ports)
+            
+        if but_port[1].overed(mouse):
+            cur_port = (cur_port+1) % len(ports)
+            
+        if but_port[2].overed(mouse) and not selected_port:  
+            try:
+                ser = serial.Serial(ports[cur_port], 115200)
+                but_port[2].color = (0, 255, 0)
+                but_port[2].text = "Connected"
+                selected_port = True
+            except:
+                but_port[2].color = (255, 0, 0)
+                
+        #SF2 select
+        if but_sf[0].overed(mouse):
+            cur_sf2 = (cur_sf2-1) % len(files)
+            but_sf[2].color = white
+            
+        if but_sf[1].overed(mouse):
+            cur_sf2 = (cur_sf2+1) % len(files)
+            but_sf[2].color = white
+         
+        if but_sf[2].overed(mouse): 
+            synth.load_sound_font('soundfonts/' + files[cur_sf2])
+            but_sf[2].color = (0, 255, 0)
+            instrument = 0
+            synth.set_instrument(0, 0)
+            
+
+         
+         #Instrument select   
+        if but_ins[0].overed(mouse):
+            instrument = (instrument-1) % 256
+            synth.set_instrument(0, instrument)
+             
+        if but_ins[1].overed(mouse):
+            instrument = (instrument+1) % 256 
+            synth.set_instrument(0, instrument)
+                
+    m_cl = False
+    
+    # Drawning
+    draw_text(screen, "Preset:", 32, 25, 100)
+    draw_text(screen, files[cur_sf2][:-4], 32, 25, 150)
+    draw_text(screen, "Port: "+ports[cur_port], 32, 25, 25)
+    draw_text(screen, "Note: "+str(last_note), 32, 525, 100)
+    draw_text(screen, "Instrument: "+str(instrument), 32, 525, 150)
+    
+    for i in but_port: i.draw()
+    for i in but_sf: i.draw()  
+    for i in but_ins: i.draw() 
+    pygame.display.flip()
+    
+    # Read piano
+    if selected_port:
+        if(ser.in_waiting >0):
+            cmd, pitch, velocity = ser.read(), ser.read(), ser.read()
+            if (cmd[0]==144):
+                if (velocity[0]==127):
+                    last_note = pitch[0]
+                    synth.play_event(pitch[0],0,100)
+                    
+                if (velocity[0]==0):
+                    synth.play_event(pitch[0],0,0)
+      
+    
+                
+    
+	
+pygame.quit()
